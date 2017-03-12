f7342ebafa61080de46f18fe5d9ce8896ce717b1
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2017-03-12 23:38:42 +0530

    Imporved detection algorithm

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -4,7 +4,7 @@ const isToughWord = word => !!dictionary[word];
 
 const lookupWord = word => dictionary[word];
 
-const wordsFromPara = $paragraph => $paragraph.innerText.split(' ');
+const wordsFromPara = paragraph => paragraph.split(' ');
 
 const annotateToughWord = word => {
     if(isToughWord(word)) {
@@ -13,14 +13,33 @@ const annotateToughWord = word => {
     return word;
 }
 
-const init = () => {
-    const $paragraphs = document.querySelectorAll('p, li');
+const isValidTextNode = $node => {
+  return $node.nodeType === $node.TEXT_NODE && $node.textContent.trim();
+}
 
-    Array.from($paragraphs).forEach($paragraph => {
-        const words = wordsFromPara($paragraph);
-        const annotatePara = words.map(annotateToughWord).join(' ');
-        $paragraph.innerHTML = annotatePara;
-    });
+const traverse = $node => {
+
+  if(['SCRIPT', 'RUBY'].some(tag => tag === $node.tagName)) return;
+
+  if(isValidTextNode($node)) {
+    const words = wordsFromPara($node.textContent.trim());
+    const annotatedPara = words.map(annotateToughWord).join(' ');
+
+    const $paragraph = document.createElement('div');
+    $paragraph.innerHTML = annotatedPara;
+
+    $node.replaceWith(...$paragraph.childNodes);
+
+    return;
+  }
+
+  if($node.hasChildNodes()) {
+    $node.childNodes.forEach(traverse);
+  }
+}
+
+const init = () => {
+    traverse(document.body);
 }
 
 // Some websites refresh data to add markup (For eg, Medium adds highlights asynchronously)
